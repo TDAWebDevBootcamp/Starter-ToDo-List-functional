0963a0734300ea40571b3c7540ba9ae7e44731b5
sarahmibrahim <sarah.m.ibrahim@outlook.com>
2022-05-16 16:05:11 +0100

    changes by Sarah I

--- src/View.js
+++ src/View.js
@@ -1,14 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Table from 'react-bootstrap/Table';
 import './App.css';
 
-function View(){
-    const [todos, changeTodos] = useState( [{ id: 1, task: "make static data", complete: false },
-    { id: 2, task: "make dynamic data", complete: false }])
-
+function View(props){
 
   const buildRows = () =>  {
-    return todos.map((current) => (
+    return props.todos.map((current) => (
       <tr key={current.id}>
         <td>
           {current.id}
