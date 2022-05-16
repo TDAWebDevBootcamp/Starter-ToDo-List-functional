bba3db01abdd4babba28e89d6adcf72ca12e6223
sarahmibrahim <sarah.m.ibrahim@outlook.com>
2022-05-16 16:24:11 +0100

    Update Add.js

--- src/Add.js
+++ src/Add.js
@@ -1,41 +1,98 @@
-import React from 'react';
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
+function Add(props) {
+  const [formValues, changeFormValues] = useState({
+    id: 0,
+    description: "",
+    completed: false,
+  });
 
-function Add(){
-  
+  const handleIdChange = (event) => {
+    const newState = { ...formValues };
+    newState.id = event.target.value;
+    changeFormValues(newState);
+  };
+
+  const handleDescriptionChange = (event) => {
+    const newState = { ...formValues };
+    newState.description = event.target.value;
+    changeFormValues(newState);
+  };
+  const handleCompletedChange = (event) => {
+    const newState = { ...formValues };
+    newState.completed = !formValues.completed;
+    changeFormValues(newState);
+  };
+  /*
+  const handleChange= (event) =>{
+    const newState={...formValues};
+    if (event.target.name === "completed"){
+      newState[event.target.name] = !formValues.completed;
+    } else{
+      newState[event.target.name] =event.target.value;
+    }
+    console.log(newState)
+  changeFormValues(
+    newState
+  )}
+  */
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+    props.onSubmit(formValues.id, formValues.description, formValues.completed);
+    changeFormValues({
+      id: 0,
+      description: "",
+      completed: false,
+    });
+
+    //need to reset the fields to default
+ 
+    event.target.elements["id"].value = "";
+    event.target.elements['description'].value = "";
+    event.target.elements['completed'].checked = false;
+
+    //document.getElementByName('description').value = "";
+  };
+
   return (
-      <div>
-        <Form>
-          <Form.Group controlId="taskID">
-            <Form.Label> Task ID</Form.Label>
-            <Form.Control 
-              name="id" 
-              type="number" 
-            />
-          </Form.Group>
-
-          <Form.Group controlId="taskDescription">
-            <Form.Label> Description</Form.Label>
-            <Form.Control 
-              name="description" 
-              type="text" 
-            />
-          </Form.Group>
-
-          <Form.Check 
-            type="checkbox"
-            id="complete"
-            label="Completed?"
+    <div>
+      <Form onSubmit={(event) => submitHandler(event)}>
+        <Form.Group controlId="taskID">
+          <Form.Label> Task ID</Form.Label>
+          <Form.Control
+            name="id"
+            type="number"
+            onChange={(event) => handleIdChange(event)}
           />
+        </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+        <Form.Group controlId="taskDescription">
+          <Form.Label> Description</Form.Label>
+          <Form.Control
+            name="description"
+            type="text"
+            onChange={(event) => handleDescriptionChange(event)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="completed">
+          <Form.Check
+            type="checkbox"
+            name="completed"
+            label="Completed?"
+            onChange={(event) => handleCompletedChange(event)}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
-
 }
+
 export default Add;
--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import './App.css';
 import View from './View'
 import Add from './Add';
 
-function App(){
+function App(props){
+  const [todos, changeTodos] = useState( [
+    { id: 1, task: "make static data", complete: false },
+    { id: 2, task: "make dynamic data", complete: false }
+  ])
+  const updateTodoItems = (id,description,complete) => {
+     const item ={id, task: description, complete};
+     changeTodos((state)=> [...state, item]);
+  }
 
     return (
         <Container>
-          <View />
-          <Add />
+          <View todos={todos} />
+          <Add onSubmit={(id,description,complete) =>
+          updateTodoItems(id,description,complete)}/>
         </Container>
     );
 
